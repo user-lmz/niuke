048fc6e55f034d06f547f56c603a03fb1b6b50ba
user-lmz <mingzeluo888@gmail.com>
2022-04-06 11:00:47 +0800

    second commit

--- src/HJ71.java
+++ src/HJ71.java
@@ -1,50 +1,43 @@
-import java.io.*;
+import java.util.Scanner;
 
 public class HJ71 {
-
-    public static void main(String[] args) throws IOException{
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        String str="";
-        while((str=bf.readLine())!=null){
-            str=str.toLowerCase();
-            String s=bf.readLine().toLowerCase();
-            boolean [][] flag=new boolean[str.length()+1][s.length()+1];
-            flag[0][0]=true;
-            if(str.charAt(0)=='*'){
-                flag[1][0]=true;
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while (in.hasNextLine()) {
+            String p = in.nextLine().toLowerCase();
+            String s = in.nextLine();
+            int m = p.length();
+            int n = s.length();
+            boolean[][] dp = new boolean[m+1][n+1];
+            dp[0][0] = true;
+            for (int i = 1; i <= m; i++) {
+                if (p.charAt(i-1) == '*') {
+                    dp[i][0] = true;
+                } else
+                    break;
             }
-            for(int i=1;i<=str.length();i++){
-                char ch=str.charAt(i-1);
-                for(int j=1;j<=s.length();j++){
-                    char c=s.charAt(j-1);
-                    if(ch=='?'){
-                        if(check(c)){
-                            flag[i][j]=flag[i-1][j-1];
-                        }else{
-                            flag[i][j]=false;
-                        }
-                    }else if(ch=='*'){
-                        if(check(c)){
-                            flag[i][j]=flag[i-1][j-1]||flag[i][j-1]||flag[i-1][j];
-                        }else{
-                            flag[i][j]=false;
-                        }
-                    }else if(ch==c){
-                        flag[i][j]=flag[i-1][j-1];
-                    }else{
-                        flag[i][j]=false;
+            for (int i = 1; i <= m; i++) {
+                char c1 = p.charAt(i-1);
+                for (int j = 1; j <= n; j++) {
+                    char c2 = s.charAt(j-1);
+                    if (c1 == '*') {
+                        if (check(c2))
+                            dp[i][j] = dp[i-1][j] || dp[i][j-1];
+                        else
+                            dp[i][j] = false;
+                    } else if (c1 == '?' || c1 == c2) {
+                        if (check(c2))
+                            dp[i][j] = dp[i-1][j-1];
+                        else
+                            dp[i][j] = false;
                     }
                 }
             }
-            System.out.println(flag[str.length()][s.length()]);
+            System.out.println(dp[m][n]);
         }
     }
 
-    public static boolean check(char ch){
-        if(ch>='a'&&ch<='z'||ch>='0'&&ch<='9'){
-            return true;
-        }
-        return false;
+    private static boolean check(char c) {
+        return c >= '0' && c <= '9' || c >= 'a' && c <= 'z';
     }
-
 }
