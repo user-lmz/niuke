8332ac3d4cc81b20688c47d39d48581cec9a7b03
user-lmz <mingzeluo888@gmail.com>
2022-04-16 10:21:36 +0800

    second commit

--- src/HJ19.java
+++ src/HJ19.java
@@ -7,20 +7,19 @@ public class HJ19 {
     public static void main(String[] args) throws IOException {
         BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
         String str;
-        LinkedHashMap<String,Integer> data = new LinkedHashMap<>();
-        while((str = buffer.readLine())!=null && !str.equals("")){
-            int idx1 = str.lastIndexOf(" ");
-            int idx2 = str.lastIndexOf("\\");
-            String key = (idx1-idx2)>16?str.substring(idx1-16):str.substring(idx2+1);
-            data.put(key,data.getOrDefault(key,0)+1);
+        LinkedHashMap<String, Integer> map = new LinkedHashMap<>();
+        while ((str = buffer.readLine())!=null && !str.equals("")) {
+            int ind1 = str.lastIndexOf(" ");
+            int ind2 = str.lastIndexOf("\\");
+            String key = (ind1 - ind2) <= 16 ? str.substring(ind2+1) : str.substring(ind1-16);
+            map.put(key, map.getOrDefault(key, 0)+1);
         }
         buffer.close();
-        int count=0;
-        for (String key:data.keySet()){
-            count++;
-            if(count>(data.size()-8)){
-                System.out.println(key+" "+data.get(key));
-            }
+        int count = 0;
+        for (String s: map.keySet()){
+            ++count;
+            if (count > map.size()-8)
+                System.out.println(s+" "+map.get(s));
         }
     }
 }
